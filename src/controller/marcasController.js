579f962ef1ec13c0256c83e2a054e4e69279fa1c
Emerson Duarte <raasjava@gmail.com>
2021-02-14 23:55:16 -0300

    Continuando com o projeto

--- src/controller/marcasController.js
+++ src/controller/marcasController.js
@@ -2,19 +2,33 @@ var connection = require ('../database/connection');
 
 module.exports = {
     async index(req, res){
-        const marcas = await connection('marca').select('*');
-        return res.json(marcas);
+        const nome = req.params.nome;
+        if(nome){
+            
+            const marcas = await connection('marca').where('nome', nome);
+            return res.json(marcas);    
+
+        } else {
+            
+            const marcas = await connection('marca').select('*');
+            return res.json(marcas);
+        }
+        
     },
 
     async create(req, res){
         const {nome, descricao, datacriacao} = req.body;
+        try{
+            await connection('marca').insert({
+                nome,
+                descricao,
+                datacriacao
+            })
+            return res.status(200).send({sucesso: 'Registro incluído com sucesso!'});
         
-        await connection('marca').insert({
-            nome,
-            descricao,
-            datacriacao
-        })
-        return res.status(200).send('Registro incluído com sucesso!')
+        } catch(err){
+            res.status(400).send({ error: 'Falha no cadastramento'});
+        }
     }
     
 };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const marcasController = require('./controller/marcasController');
 const routes = express.Router();
 
 routes.get('/marcas', marcasController.index);
+routes.get('/marcas/:nome', marcasController.index);
 routes.post('/marcas', marcasController.create);
 
 module.exports = routes;
